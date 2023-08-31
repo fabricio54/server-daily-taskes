3b02f4aa8f2ac6c9460fad14b8e9d58c7f0dda21
Fabricio Alves <fabricioallves2020@gmail.com>
2023-08-31 16:38:21 -0300

    resolvendo bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -21,7 +21,7 @@ export const userCreateController = async (req, res) => {
         }
 
         // mostrando o usuário criado na tela
-        return res.send({
+        return res.status(200).send({
             message: "usuário criado com sucesso",
             user: {
                 name,
