13f93ab990fdf5e0e7c4e001e0201f6844999973
Fabricio Alves <fabricioallves2020@gmail.com>
2023-09-22 13:42:56 -0300

    adicionado rota para confirmar email existente de usuário

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,7 @@
 import { userCreateService } from "../services/user.service.js";
+import { transport
+} from "../services/auth.service.js";
+import { randomMath } from "../services/functionSimples.js";
 
 export const userCreateController = async (req, res) => {
     try {
@@ -9,7 +12,6 @@ export const userCreateController = async (req, res) => {
         if (!name || !username || !email || !password) {
             res.status(400).send({ message: "Envie todos os campos para registro" });
         }
-
         // criando um serviço para criação de contas de usuários
         const user = await userCreateService(req.body);
 
@@ -33,4 +35,35 @@ export const userCreateController = async (req, res) => {
     } catch (error) {
         res.status(500).send({ message: error.message });
     }
+}
+
+export const isEmailValid = async (req, res) => {
+   try {
+       // pegando os dados do usuário no body
+       const { name, email } = req.body;
+
+       // validando dados
+       if (!name || !email) {
+           res.status(400).send({ message: "Envie todos os campos para registro" });
+       }
+
+       const codigo = randomMath();
+
+       transport.sendMail({
+           from: "Daily Tasks",
+           to: email,
+           subject: "Enviando email com Nodemailer",
+           html: `<h1>Olá, ${name}!</h1><p>Código de verificação ${codigo}`,
+           text: `olá, ${name}! Esse email foi enviado para conclusão de cadastro no Daily Taskes`
+       })
+       .then(() => console.log("email enviado com sucesso!"))
+       .catch((error) => console.log(error));
+
+       req.codigo = codigo;
+
+   } catch (error) {
+       res.status(500).send({
+           message: error.message
+       })
+   }
 }
